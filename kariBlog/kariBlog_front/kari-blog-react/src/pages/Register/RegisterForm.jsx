0c0de4fe898c56f17246d777c558e1db3c742f95
minji0000 <bluurmi@gmail.com>
2022-10-08 01:16:38 +0900

    2022 10 08 back register 구현중

--- kariBlog/kariBlog_front/kari-blog-react/src/pages/Register/RegisterForm.jsx
+++ kariBlog/kariBlog_front/kari-blog-react/src/pages/Register/RegisterForm.jsx
@@ -5,45 +5,44 @@ import "../Home/MainCss.css";
 
 function RegisterForm() {
   return (
-    <div className='main register'>
+    <div className='main register' >
     
       <img id="logo_column" className="container" src={Logo} />
 
       <div className='container text-center'>
 
-        <form class="form-control">
+        <form class="form-control" method='post'>
           <h3>ID / PASSWORD </h3>
           <div className='m-1'>
-            아이디 <input className='btn-kari m-1' />   
+            아이디 <input name='username' className='btn-kari m-1' />   
             <button className='btn-kari btn1'> 중복체크 </button> 
           </div>
-          비밀번호 <input className='btn-kari mb-2'/>
+          비밀번호 <input name='password' className='btn-kari mb-2'/>
           <br/>
 
           <br/>
           <br/>
           <h3> PROFILE </h3>
-          이름 <input className='btn-kari mb-2'/>
+          이름 <input name='name' className='btn-kari mb-2'/>
           <br/>
-          생년월일 <input type="date" className='btn-kari mb-2'/>
+          생년월일 <input name='birth' type="date" className='btn-kari mb-2'/>
           <br/>
-          전화번호 <input type="tel" className='btn-kari col-md-1 mb-2'/>
-          - <input type="tel" className='btn-kari col-md-1 mb-2'/>
-          - <input type="tel" className='btn-kari col-md-1 mb-2'/>
+          전화번호 <input name='number1' type="tel" className='btn-kari col-md-1 mb-2'/>
+          - <input name='number2' type="tel" className='btn-kari col-md-1 mb-2'/>
+          - <input name='number3' type="tel" className='btn-kari col-md-1 mb-2'/>
           <br/>
-          이메일 <input className='btn-kari col-md-5 mb-2'/>
+          이메일 <input name='email' className='btn-kari col-md-5 mb-2'/>
           <br/>
 
           <br/>
           <br/>
           <h3> KARI BLOG PROFILE </h3>
-          닉네임 <input className='btn-kari mb-3'/>
+          닉네임 <input name='nickname' className='btn-kari mb-3'/>
           <br/>         
           
-          <button className='btn-kari btn1 col-md-4'> 회원가입 </button>
+          <button type='submit' className='btn-kari btn1 col-md-4'> 회원가입 </button>
         
-
-          
+        
         
         </form>
       
