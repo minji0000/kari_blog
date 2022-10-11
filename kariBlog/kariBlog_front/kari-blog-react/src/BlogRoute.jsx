69653612641ed5047a250f8f48cf5b431e6a94b8
minji0000 <bluurmi@gmail.com>
2022-10-11 22:38:11 +0900

    2022 10 11 front 회원가입 중

--- kariBlog/kariBlog_front/kari-blog-react/src/BlogRoute.jsx
+++ kariBlog/kariBlog_front/kari-blog-react/src/BlogRoute.jsx
@@ -13,7 +13,6 @@ function BlogRoute() {
 
 
 
-
       </Routes>
 
 
--- kariBlog/kariBlog_front/kari-blog-react/src/pages/Home/Home.jsx
+++ kariBlog/kariBlog_front/kari-blog-react/src/pages/Home/Home.jsx
@@ -3,6 +3,9 @@ import Register from "../Register/Register";
 import "./Home.css";
 import LogoDog from './/logo_dog.jpg'
 import Login from "../../components/Login/Login";
+import BlogMap from "../Map/MapApi";
+
+
 
 function Home() {
   return (
@@ -10,7 +13,8 @@ function Home() {
       <img id="logo_dog" className="container" src={LogoDog} />
       <Login></Login>
       <Register></Register>
-      
+      <BlogMap></BlogMap>
+
     </div>
   );
 }
