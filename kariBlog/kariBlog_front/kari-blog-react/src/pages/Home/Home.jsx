b49875b2a9d67dd357dceac03fbfa9e280342dcc
minji0000 <bluurmi@gmail.com>
2022-10-14 15:27:53 +0900

    2022 10 14 KAKAO MAP API front

--- kariBlog/kariBlog_front/kari-blog-react/src/pages/Home/Home.jsx
+++ kariBlog/kariBlog_front/kari-blog-react/src/pages/Home/Home.jsx
@@ -3,9 +3,6 @@ import Register from "../Register/Register";
 import "./Home.css";
 import LogoDog from './/logo_dog.jpg'
 import Login from "../../components/Login/Login";
-import BlogMap from "../Map/MapApi";
-
-
 
 function Home() {
   return (
@@ -13,8 +10,6 @@ function Home() {
       <img id="logo_dog" className="container" src={LogoDog} />
       <Login></Login>
       <Register></Register>
-      <BlogMap></BlogMap>
-
     </div>
   );
 }
